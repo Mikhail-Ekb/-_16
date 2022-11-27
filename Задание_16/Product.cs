12f8ea181baccdde038cd4b3ecb7d46760df4235
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-28 00:05:45 +0500

    Добавьте файлы проекта.

---
+++ Задание_16/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Задание_16
+{
+    internal class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Задание_16/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Задание_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            Product[] products = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите код товара: ");
+                int code = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите название товара: ");
+                string name = Console.ReadLine();
+                Console.Write("Введите цену товара: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+
+                products[i] = new Product { Code = code, Name = name, Price = price };
+            }
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(products, options);
+
+            using (StreamWriter sw = new StreamWriter("../../../Products.json"))
+            {
+                sw.WriteLine(jsonString);
+            }
+
+        }
+    }
+}
